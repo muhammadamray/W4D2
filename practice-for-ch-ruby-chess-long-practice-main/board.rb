a106b8a2cc782da01add1dd0036c3d7092c65f26
muhammadamray <muhammadamray@gmail.com>
2023-07-19 11:36:24 -0400

    add piece and valid position added

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -14,10 +14,28 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
-        if self[start_pos].empty?
-            raise 'error'
-        end
+        raise 'No piece at starting pos' if self[start_pos].empty?
+        rasie 'not a valid end pos' if !self.valid_pos?(end_pos)
+
+        
+        
     end
+
+    def valid_pos?(pos)
+        pos.all? { |coord| coord.between?(0, 7) }
+    end
+    
+    def add_piece(piece, pos)
+        self[pos] = piece
+    end
+
+
+
+
+
+
+
+
 
 end
 
--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,5 +1,23 @@
 class Piece 
-    def initialize(bishop, rook, queen, knight, king)
-        
-    end
+    
+    attr_reader :color, :board
+    attr_accessor :pos
+
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+
+    board.add_piece(self, pos)
+  end
+
+
+
+
+
+
+
+
+
+
 end
